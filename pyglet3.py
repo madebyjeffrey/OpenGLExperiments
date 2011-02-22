47268eefe186b67c98cd7744c8c3115e0aabba04
Jeffrey Drake <iaefai@me.com>
2011-02-21 23:21:30 -0500

    Vertex Buffer Object added to 3

--- pyglet3.py
+++ pyglet3.py
@@ -1,3 +1,4 @@
+from __future__ import division
 
 from pyglet import clock, font, image, window, text
 from pyglet.gl import *
@@ -11,29 +12,45 @@ TWO_PI = 2.0 * 3.1415926535
 
 class World(object):
     def __init__(self):
-        self.mesh = numpy.zeros((SIZE, SIZE, 3), 'f')
-        
+        self.verts_id = GLuint()
+        glGenBuffers(1, self.verts_id)
+        self.mesh = []
         for x in xrange(SIZE):
             for z in xrange(SIZE):
-                self.mesh[x][z][0] = float ((SIZE/2) -x) # centre around origin
-                self.mesh[x][z][1] = 0.0;
-                self.mesh[x][z][2] = float((SIZE / 2) -z)
+                self.mesh.extend((float(SIZE//2 - x), 0.0, float(SIZE//2 - z)))
+        data = (GLfloat*len(self.mesh))(*self.mesh)
+        glBindBuffer(GL_ARRAY_BUFFER, self.verts_id)
+        glBufferData(GL_ARRAY_BUFFER, len(data)*4, data, GL_STATIC_DRAW)       
+
+        self.index_id = GLuint()
+        glGenBuffers(1, self.index_id)
+        self.index = []
+        for x in xrange(SIZE-1):
+            for z in xrange(SIZE-1):
+                self.index.extend((x*SIZE+z, x*SIZE+z+1, (x+1)*SIZE+z))
+                self.index.extend((x*SIZE+z+1, (x+1)*SIZE+z, (x+1)*SIZE+z+1))
+        data = (GLushort*len(self.index))(*self.index)
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, self.index_id)
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, len(data)*2, data, GL_STATIC_DRAW)        
+
         
     def draw(self):
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         glLoadIdentity()
     
         gluLookAt(0.0, 25.0, -45.0, 0.0, 0.0, 0.0, 0, 1, 0)
     
-        for x in xrange (SIZE - 1):
-            glBegin(GL_TRIANGLE_STRIP)
-            for z in xrange (SIZE - 1):
-                glVertex3f(self.mesh[x][z][0], self.mesh[x][z][1], self.mesh[x][z][2])
-                glVertex3f(self.mesh[x+1][z][0], self.mesh[x+1][z][1], self.mesh[x+1][z][2])
-                
-            glEnd()
+        glEnableClientState(GL_VERTEX_ARRAY)
+        glBindBuffer(GL_ARRAY_BUFFER, self.verts_id)
+        glVertexPointer(3, GL_FLOAT, 0, 0)
+
+        glEnableClientState(GL_INDEX_ARRAY)
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, self.index_id)
+        glDrawElements(GL_TRIANGLES, len(self.index), GL_UNSIGNED_SHORT, 0)
+
         glFlush()
-        
+                
 
 
 class Camera(object):
@@ -57,11 +74,12 @@ class Camera(object):
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
         gluOrtho2D(0, self.win.width, 0, self.win.height)
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
     
 class Hud(object):
     def __init__(self, win, world):
         helv = font.load('Helvetica', 30)
-        self.fpsLabel = text.Label('', font_name='Helvetica', x=10, y=10)
+        self.fpsLabel = text.Label('', font_name='Helvetica', x=60, y=60)
         self.fps = clock.ClockDisplay(
             format = "%(fps).1ffps", font=helv)
         self.fps.label = self.fpsLabel
@@ -69,7 +87,9 @@ class Hud(object):
     def draw(self):
         glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
-        self.fps.draw()
+        self.fpsLabel.text = ("%.1f" % clock.get_fps())
+        self.fpsLabel.draw()
+        
 
 class App(object):
     def __init__(self):
@@ -77,7 +97,7 @@ class App(object):
         self.config = Config(double_buffer = True)
         self.win = window.Window(800, 600, 
                                 caption="Terrain 3",
-                                vsync = True,
+                                vsync = False,
                                 config = self.config)
         self.camera = Camera(self.win, self.world, zoom=10)
         self.hud = Hud(self.win, self.world)
@@ -90,6 +110,7 @@ class App(object):
 #            self.win.clear()
             #dt = clock.tick()
             #self.world.tick(dt)
+            clock.tick()
             
             self.camera.worldProjection()
             self.world.draw()
